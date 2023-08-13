76568315fe7ad63a1c0f5bc248dda91989eb559c
Vladislav Akashev <vlad.akashev.w@gmail.com>
2023-08-13 14:44:11 +0300

    Fix linter remarks

--- src/gendiff.py
+++ src/gendiff.py
@@ -3,10 +3,13 @@ import json
 
 
 def setup_parser():
-    parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
+    parser = argparse.ArgumentParser(
+        description='Compares two configuration files and shows a difference.')
     parser.add_argument('first_file')
     parser.add_argument('second_file')
-    parser.add_argument('-f', '--format', help='set format of output')
+    parser.add_argument('-f',
+                        '--format',
+                        help='set format of output')
     args = parser.parse_args()
     return args
 
