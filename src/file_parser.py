3fcf0d1b5384b9d49a6a0e2fcdb2f60bd9015b74
Vladislav Akashev <vlad.akashev.w@gmail.com>
2024-04-04 23:25:36 +0300

    Refactor get_file_parser function

--- src/file_parser.py
+++ src/file_parser.py
@@ -2,9 +2,30 @@ import json
 import yaml
 
 
-# TODO: Add contex manager for loading
-def get_file_parser(file_format):
-    if file_format == 'JSON':
-        return lambda file_path: json.load(open(file_path))
-    elif file_format == 'YAML':
-        return lambda file_path: yaml.safe_load(open(file_path))
+def load_json(file_path):
+    with open(file_path, 'r') as file:
+        try:
+            return json.load(file)
+        except json.JSONDecodeError as e:
+            print(f"Error loading JSON from file '{file_path}': {e}")
+            return None
+
+
+def load_yaml(file_path):
+    with open(file_path, 'r') as file:
+        try:
+            return yaml.safe_load(file)
+        except yaml.YAMLError as e:
+            print(f"Error loading YAML from file '{file_path}': {e}")
+
+            return None
+
+
+def get_file_parser(file_extension):
+    if file_extension == 'json':
+        return lambda file_path: load_json(file_path)
+    elif file_extension in ['yaml', 'yml']:
+        return lambda file_path: load_yaml(file_path)
+    else:
+        print(f"Unsupported file extension: '{file_extension}'")
+        return None
